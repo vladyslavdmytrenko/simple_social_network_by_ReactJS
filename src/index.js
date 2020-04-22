561e70e4ec6d3dda23d2b80e0647fcec253e0917
vladyslavdmytrenko <vladyslavdmytrenko96@gmail.com>
2020-04-22 22:16:51 +0300

    rewrite state to OOP

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import state from './redux/state';
-import {rerender} from './render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import App from './App';
+import store from './redux/state';
 
-rerender(state);
+export let rerender = (props)=> {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App
+                    addPost={store.addPost.bind(store)}
+                    updateNewPostData={store.updateNewPostData.bind(store)}
+                    addMessage={store.addMessage.bind(store)}
+                    updateNewMessageData={store.updateNewMessageData.bind(store)}
+
+                    dataProfile={props.dataProfile}
+                    dataFriends={props.friends}
+                    dataDialogs={props.dataDialogs}
+                    dataMessages={props.dataMessages}
+                />
+            </BrowserRouter>
+        </React.StrictMode>
+        ,
+        document.getElementById('root')
+    )};
+
+rerender(store.getState());
+
+store.subscribe(rerender);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,52 +1,111 @@
-import {rerender} from '../render';
-
-const state = {
-    friends:[
-        {name:'Name 1', id:1, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 2', id:2, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 3', id:3, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 4', id:4, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-    ],
-    dataProfile:{
-        textNewPostData:'',
-        dataProfileItems:[
-        {name:'Name 1', likes: 3, message:'Hello', id:1, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'},
-        {name:'Name 2', likes: 5, message:'Hello', id:2, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'},
-        {name:'Name 3', likes: 0, message:'Hello', id:3, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'},
-    ]},
-    dataDialogs:[
-        {name:'Name 1', id:1, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 2', id:2, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 3', id:3, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-        {name:'Name 4', id:4, img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'},
-    ],
-    dataMessages:{
-        textNewMessageData: '',
-        dataMessagesItems:[
-            {massage:'Massage 1', id:1, interlocutor: true},
-            {massage:'Massage 2', id:2, interlocutor: true},
-            {massage:'Massage 3', id:3, interlocutor: false},
-            {massage:'Massage 4', id:4, interlocutor: true}
-        ]}
+const store = {
+    _state: {
+        friends: [
+            {
+                name: 'Name 1',
+                id: 1,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 2',
+                id: 2,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 3',
+                id: 3,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 4',
+                id: 4,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+        ],
+        dataProfile: {
+            textNewPostData: '',
+            dataProfileItems: [
+                {
+                    name: 'Name 1',
+                    likes: 3,
+                    message: 'Hello',
+                    id: 1,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'
+                },
+                {
+                    name: 'Name 2',
+                    likes: 5,
+                    message: 'Hello',
+                    id: 2,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'
+                },
+                {
+                    name: 'Name 3',
+                    likes: 0,
+                    message: 'Hello',
+                    id: 3,
+                    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'
+                },
+            ]
+        },
+        dataDialogs: [
+            {
+                name: 'Name 1',
+                id: 1,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 2',
+                id: 2,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 3',
+                id: 3,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+            {
+                name: 'Name 4',
+                id: 4,
+                img: 'https://image.shutterstock.com/image-vector/user-sign-icon-person-symbol-260nw-229318795.jpg'
+            },
+        ],
+        dataMessages: {
+            textNewMessageData: '',
+            dataMessagesItems: [
+                {massage: 'Massage 1', id: 1, interlocutor: true},
+                {massage: 'Massage 2', id: 2, interlocutor: true},
+                {massage: 'Massage 3', id: 3, interlocutor: false},
+                {massage: 'Massage 4', id: 4, interlocutor: true}
+            ]
+        }
+    },
+    getState() {
+      return this._state;
+    },
+    rerender(state) {
+        console.log(state);
+    },
+    addPost() {
+        this._state.dataProfile.dataProfileItems.push({name:'Name 1', likes: 3, message:this._state.dataProfile.textNewPostData, id:1, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'});
+        this._state.dataProfile.textNewPostData = '';
+        this.rerender(this._state);
+    },
+    updateNewPostData(newText) {
+        this._state.dataProfile.textNewPostData = newText;
+        this.rerender(this._state);
+    },
+    addMessage() {
+        this._state.dataMessages.dataMessagesItems.push({massage:this._state.dataMessages.textNewMessageData, id:1, interlocutor: false});
+        this._state.dataMessages.textNewMessageData = '';
+        this.rerender(this._state);
+    },
+    updateNewMessageData(newText) {
+        this._state.dataMessages.textNewMessageData = newText;
+        this.rerender(this._state);
+    },
+    subscribe(observer) {
+        this.rerender = observer;
+    }
 };
-
-export let addPost = () => {
-    state.dataProfile.dataProfileItems.push({name:'Name 1', likes: 3, message:state.dataProfile.textNewPostData, id:1, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRovSSDmSPldW50frhQXofGaJOWTN9nWwrDdhrn77Bl0gt75nyv&usqp=CAU'});
-    state.dataProfile.textNewPostData = '';
-    rerender(state)
-};
-export let updateNewPostData= (newText) => {
-    state.dataProfile.textNewPostData = newText;
-    rerender(state);
-};
-
-export let addMessage = () => {
-    state.dataMessages.dataMessagesItems.push({massage:state.dataMessages.textNewMessageData, id:1, interlocutor: false});
-    state.dataMessages.textNewMessageData = '';
-    rerender(state);
-};
-export let updateNewMessageData = (newText) => {
-    state.dataMessages.textNewMessageData = newText;
-    rerender(state);
-};
-export default state
+export default store
